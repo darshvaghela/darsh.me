de340aa1c3f5fa32259f3845c56977dbeff6a1c7
Vaghela Darsh <77739833+darshvaghela@users.noreply.github.com>
2026-05-02 11:04:04 +0530

    Fix type casting for block in NotionPage component

--- components/NotionPage.tsx
+++ components/NotionPage.tsx
@@ -188,7 +188,7 @@ export const NotionPage: React.FC<types.PageProps> = ({
   // const isRootPage =
   //   parsePageId(block?.id) === parsePageId(site?.rootNotionPageId)
   const isBlogPost =
-    block?.type === 'page' && block?.parent_table === 'collection'
+    (block as any)?.type === 'page' && (block as any)?.parent_table === 'collection'
 
   const showTableOfContents = !!isBlogPost
   const minTableOfContentsItems = 3
